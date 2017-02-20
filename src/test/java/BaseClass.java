213bb2f5f39f2075e31ac90a65ef87766a346653
amish44 <amish.choudhary@icann.org>
2017-02-20 11:41:59 +0530

    Added log4j, take screenshot on failure

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -3,8 +3,11 @@ package test.java;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
 
 /**
  * Created by achoudhary on 2/17/17.
@@ -18,6 +21,13 @@ public class BaseClass {
         System.setProperty("webdriver.chrome.driver", "G:\\ICANN\\dev\\Test1\\src\\test\\resources\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    void tearDown(ITestResult result){
+        if(ITestResult.FAILURE==result.getStatus()){
+            Utility.takeScreenshot(driver,result.getName());
+        }
     }
 
     @AfterClass
--- src/test/java/TestingSelenium1.java
+++ src/test/java/TestingSelenium1.java
@@ -1,24 +1,34 @@
 package test.java;
 
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+
 
 /**
  * Created by achoudhary on 2/17/17.
  */
 public class TestingSelenium1 extends BaseClass{
 
-
+    private static Logger logger = Logger.getLogger(TestingSelenium1.class.getName());
     @Test(priority = 1)
     public void test1()
     {
+        DOMConfigurator.configure("log4j.xml");
+        logger.info("Test1 started");
         driver.get("https://google.co.in");
+        driver.findElement(By.className("wrongfortesting")).click();
         System.out.println("google here");
-
+        logger.info("Test1 ended");
     }
 
     @Test
     public void test2()
     {
+        logger.info("ancdasdfsdf");
         driver.get("https://yahoo.com");
         System.out.println("yahoo here");
 
